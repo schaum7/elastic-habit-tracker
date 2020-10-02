f63d80e65c208b077985e16173c73a1cbd0875e8
Schaum7 <falk.altrock@gmail.com>
2020-10-02 08:07:01 +0200

    save grid in localStorage

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
-const days = ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag","Samstag","Sonntag"]
-const calendar = document.querySelector(".calendar")
+const days = [
+  "Montag",
+  "Dienstag",
+  "Mittwoch",
+  "Donnerstag",
+  "Freitag",
+  "Samstag",
+  "Sonntag",
+];
+const calendar = document.querySelector(".calendar");
+let habits = {};
 
+initCalendar();
 
-for (let i = 0; i < (7*7); i++) {
-    let cell = document.createElement("div")
-    cell.classList.add("calendar-cell")
-    cell.setAttribute("id", "cell-" + i)
-    if (i<7){
-        cell.textContent = days[i]
-        cell.classList.add("calendar-head")
-    }else{
-        cell.classList.add("calendar-day")
-        cell.addEventListener("click",trackHabit)
+function initCalendar() {
+  if (JSON.parse(localStorage.getItem("habits"))) {
+    habits = JSON.parse(localStorage.getItem("habits"));
+  }
+
+  for (let i = 0; i < 7 * 7; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("calendar-cell");
+    cell.setAttribute("id", i + "");
+    if (i < 7) {
+      cell.textContent = days[i];
+      cell.classList.add("calendar-head");
+    } else {
+      cell.classList.add("calendar-day");
+      cell.addEventListener("click", trackHabit);
+      if (habits[i]) {
+        cell.classList.add(habits[i]);
+      }
     }
-    calendar.appendChild(cell)  
+    calendar.appendChild(cell);
+  }
 }
 
 function trackHabit() {
-    if (this.classList.contains("mini")){
-        this.classList.remove("mini")
-        this.classList.add("plus")
-        
-    } else if (this.classList.contains("plus")){
-        this.classList.remove("plus")
-        this.classList.add("elite")
+  id = parseInt(this.getAttribute("id"));
 
-    }else if (this.classList.contains("elite")){
-        this.classList.remove("elite")
-        
-    } else{
-        this.classList.add("mini")
-    }
-
+  if (this.classList.contains("mini")) {
+    this.classList.remove("mini");
+    this.classList.add("plus");
+    habits[id] = "plus";
+  } else if (this.classList.contains("plus")) {
+    this.classList.remove("plus");
+    this.classList.add("elite");
+    habits[id] = "elite";
+  } else if (this.classList.contains("elite")) {
+    this.classList.remove("elite");
+    delete habits[id];
+  } else {
+    this.classList.add("mini");
+    habits[id] = "mini";
+  }
+  localStorage.setItem("habits", JSON.stringify(habits));
 }
